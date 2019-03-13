667fefa5b0bfe7461c21036178b6e941f4b33cd4
cheskyposen <shragaposen@gmail.com>
2019-03-13 13:30:25 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kosher';
+export class AppComponent implements OnInit {
+  /*title = 'kosher';*/
+  cardTitle = 'This is a simple card';
+  cardSubtitle = 'Dog Breed';
+  cardBody = 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.';
+
+  ngOnInit(): void {
+    this.cardTitle = 'WORD!!!';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { RestCardComponent } from './views/rest-card/rest-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
